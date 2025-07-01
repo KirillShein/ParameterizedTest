73b17ee641d8ac6d39d934542f1dfea63962e9fa
Kirill Shein <kshein71@yandex.ru>
2025-07-01 16:52:36 +0300

    refactoring configuration.browser

--- src/test/java/tests/JenKinsRusWineCatalog.java
+++ src/test/java/tests/JenKinsRusWineCatalog.java
@@ -24,7 +24,7 @@ public class JenKinsRusWineCatalog {
     static void beforeAll() {
         Configuration.baseUrl = "https://www.ruswine-spb.ru";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browser = System.getProperty("browser", "opera");
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
