2b6e17a7b266c46aeb2b91b1b81e42b9e43dee75
Kirill Shein <kshein71@yandex.ru>
2025-07-01 16:41:56 +0300

    added configuration.browser

--- src/test/java/tests/JenKinsRusWineCatalog.java
+++ src/test/java/tests/JenKinsRusWineCatalog.java
@@ -24,7 +24,8 @@ public class JenKinsRusWineCatalog {
     static void beforeAll() {
         Configuration.baseUrl = "https://www.ruswine-spb.ru";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = System.getProperty("driver_url");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
